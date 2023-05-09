05e1922bda9d8dcf0200cbd18e83c5132d700890
golybock <teplovartem3094@gmail.com>
2023-05-09 04:18:47 +0300

    dictionary writer

---
+++ DictionaryManager/DictionaryWriter.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using Models.Models.Dictionaries;
+
+namespace DictionaryManager;
+
+public class DictionaryWriter
+{
+    public async Task WriteCfoDictionary(List<CfoDictionary> cfos)
+    {
+        throw new NotImplementedException();
+        
+        string path = "";
+        
+        var options = new JsonSerializerOptions
+        {
+            Encoder =
+                JavaScriptEncoder.Create(
+                    UnicodeRanges.BasicLatin,
+                    UnicodeRanges.Cyrillic
+                ),
+            WriteIndented = true
+        };
+
+        string jsonCfos = JsonSerializer.Serialize(cfos, options);
+
+        if (path != null)
+        {
+            await using StreamWriter sw = new StreamWriter(path);
+
+            await sw.WriteLineAsync(jsonCfos);
+        }
+    }
+}
--- ExcelDataAnalysis/Models/Dictionary/DictionaryJsonWriter.cs
+++ ExcelDataAnalysis/Models/Dictionary/DictionaryJsonWriter.cs
@@ -4,7 +4,7 @@ using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Unicode;
 using System.Threading.Tasks;
-using ExcelParse.Models.Dictionaries;
+using Models.Models.Dictionaries;
 
 namespace ExcelDataAnalysis.Models.Dictionary;
 
